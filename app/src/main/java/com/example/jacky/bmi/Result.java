a55d3697740873477a97b62df7badb2ec59ce079
Jacky <kuoyichun1102@gmail.com>
2015-01-29 11:46:29 +0800

    first commit

---
+++ app/src/main/java/com/example/jacky/bmi/Result.java
@@ -0,0 +1,64 @@
+package com.example.jacky.bmi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+
+
+public class Result extends Activity {
+
+    private TextView BMIText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        Intent intent = getIntent();
+        double BMI = intent.getDoubleExtra("result",0);
+        BMI = new BigDecimal(BMI).setScale(2,BigDecimal.ROUND_HALF_DOWN).doubleValue();
+//        Log.d("Result", BMI);
+        BMIText = (TextView) findViewById(R.id.BMI);
+        BMIText.setText(Double.toString(BMI));
+
+        Button button_ok = (Button) findViewById(R.id.button_ok);
+        button_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_result, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
